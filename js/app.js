25945d7e25b5af39de2fe90794d43080e71c60c8
Subhradeep <subhradeep2105@gmail.com>
2021-10-10 22:49:59 +0530

    hero_section for mobile created

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const hamBurger = document.querySelector(".header__hamburger");
 const headerNav = document.querySelector(".header__wrapper")
 const toggleMenu = document.querySelector(".toggle-menu");
+
+
 hamBurger.addEventListener('click', () => {
     if (!headerNav.classList.contains("toggle")) {
         headerNav.classList.add("toggle");
         toggleMenu.style.display = "block";
+
+
 
     } else {
         headerNav.classList.remove("toggle");
