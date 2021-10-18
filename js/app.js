f758c4fa63371182744899677f485860459cc553
Subhradeep <subhradeep2105@gmail.com>
2021-10-18 18:15:34 +0530

    features-section for mobile created

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 const hamBurger = document.querySelector(".header__hamburger");
 const headerNav = document.querySelector(".header__wrapper")
 const toggleMenu = document.querySelector(".toggle-menu");
-
+let tabs = document.querySelectorAll("[data-tab-target]");
+let tabItems = document.querySelectorAll("[data-tab-content");
 
 hamBurger.addEventListener('click', () => {
     if (!headerNav.classList.contains("toggle")) {
         headerNav.classList.add("toggle");
         toggleMenu.style.display = "block";
-
-
 
     } else {
         headerNav.classList.remove("toggle");
@@ -17,3 +16,31 @@ hamBurger.addEventListener('click', () => {
     }
 })
 
+
+tabs.forEach(tab => {
+
+    tab.addEventListener('click', () => {
+
+        const target = document.querySelector(tab.dataset.tabTarget);
+
+        tabItems.forEach(item => {
+            item.classList.remove("visible");
+        })
+        tabs.forEach(element => {
+            element.classList.remove("active");
+        })
+
+        tab.classList.add("active");
+        target.classList.add("visible");
+    })
+
+});
+
+/*
+element.addEventListener("click", function () {
+    features.forEach(f => f.classList.remove("active"));
+
+    this.classList.add("active");
+
+})
+*/
